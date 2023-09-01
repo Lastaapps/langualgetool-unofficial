025a388a8849211123390e925a634303534ee706
Lasta Apps <krasik.peta@seznam.cz>
2023-09-01 16:38:17 +0300

    style: Change root src/main folder name

--- app/src/main/kotlin/cz/lastaapps/languagetool/ui/components/IconButtonTooltip.kt
+++ app/src/main/kotlin/cz/lastaapps/languagetool/ui/components/IconButtonTooltip.kt
@@ -1,7 +1,5 @@
 package cz.lastaapps.languagetool.ui.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
--- app/src/main/kotlin/cz/lastaapps/languagetool/ui/theme/generated/Theme.kt
+++ app/src/main/kotlin/cz/lastaapps/languagetool/ui/theme/generated/Theme.kt
@@ -1,11 +1,7 @@
 package cz.lastaapps.languagetool.ui.theme.generated
 
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.runtime.Composable
-
+import androidx.compose.material3.lightColorScheme
 
 
 object Tmp
